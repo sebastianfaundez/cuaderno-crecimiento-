f4d983dae88de78999167a88f352cc5930f3568c
Sebastian Faúndez <sebastian.faundez@gmail.com>
2025-12-16 21:42:44 -0300

    Ajuste static setting.py

--- betacuaderno/settings.py
+++ betacuaderno/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,9 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-#STATIC_URL = 'static/' # linea original 
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/' # linea original 
+#STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'betacuaderno/static'),)
 
 # Default primary key field type
